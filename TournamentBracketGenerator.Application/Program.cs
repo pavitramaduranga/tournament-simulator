fde173155f4f54e073e4cfdedd6889bfb779ac0d
pavitramaduranga <pavitramaduranga@gmail.com>
2023-10-20 15:49:07 +0530

    Add support for group stage Structure input

--- TournamentBracketGenerator.Application/Program.cs
+++ TournamentBracketGenerator.Application/Program.cs
@@ -31,6 +31,7 @@ class Tournament
         matchEvents = new List<MatchEvent>();
     }
 
+    #region Seed data
     public void SeedTeam(int seed, string teamName)
     {
         Team team = new Team { Name = teamName, Seed = seed };
@@ -49,7 +50,9 @@ class Tournament
             SeedTeam(seed, teamName);
         }
     }
+    #endregion
 
+    #region Tournament
     public Team GetTournamentWinner()
     {
         return teams.Count == 1 ? teams[0] : null;
@@ -105,6 +108,101 @@ class Tournament
             team2.NextRoundOpponent = team1;
         }
     }
+
+    public void SimulateTournament()
+    {
+        while (teams.Count != 1)
+        {
+            PairTeams();
+            var teamList = teams.ToList();
+            for (int i = 0; i < teamList.Count / 2; i++)
+            {
+                var team = teamList[i];
+                SimulateMatch(team, team.NextRoundOpponent);
+            }
+        }
+
+        PathToVictory(GetTournamentWinner());
+    }
+
+    #endregion
+
+    #region Grouping stage
+    public List<Team> GetTopTeams(List<Team> group, int topCount)
+    {
+        Dictionary<Team, int> pointsDictionary = new Dictionary<Team, int>();
+
+        foreach (Team team in group)
+        {
+            int points = SimulateGroupMatches(team, group);
+            pointsDictionary[team] = points;
+        }
+
+        var sortedTeams = pointsDictionary.OrderByDescending(pair => pair.Value).Select(pair => pair.Key);
+
+        List<Team> topTeams = sortedTeams.Take(topCount).ToList();
+
+        return topTeams;
+    }
+
+    private int SimulateGroupMatches(Team team, List<Team> group)
+    {
+        int points = 0;
+
+        foreach (Team opponent in group)
+        {
+            if (team != opponent)
+            {
+                Random random = new Random();
+                if (random.Next(2) == 0)
+                {
+                    points += 3;
+                }
+                else
+                {
+                    points += 1;
+                }
+            }
+        }
+
+        return points;
+    }
+
+    public void SimulateGroupStage()
+    {
+        // Break the teams into 4 groups of 4 teams each
+        var groups = new List<List<Team>>();
+        for (int i = 0; i < teams.Count; i += 4)
+        {
+            groups.Add(teams.Skip(i).Take(4).ToList());
+        }
+
+        Console.WriteLine("Group Stage");
+
+        // Simulate group matches and get the top 2 teams from each group
+        List<Team> topTeams = new List<Team>();
+        foreach (var group in groups)
+        {
+            Console.WriteLine("Group Teams:");
+            foreach (var team in group)
+            {
+                Console.WriteLine(team.Name);
+            }
+
+            List<Team> groupTopTeams = GetTopTeams(group, 2);
+            topTeams.AddRange(groupTopTeams);
+        }
+
+        Console.WriteLine("\nTop Teams from the Group Stage:");
+        foreach (var team in topTeams)
+        {
+            Console.WriteLine($"{team.Name} (Seed {team.Seed})");
+        }
+    }
+
+    #endregion
+
+
 }
 
 class Program
@@ -113,24 +211,47 @@ class Program
     {
         Tournament tournament = new Tournament();
 
-        int numberOfTeams = 64;
-
         Console.WriteLine("Tournament Dashboard");
 
-        tournament.SeedTeams(numberOfTeams);
-
-        while (tournament.teams.Count != 1)
+        int option = 0;
+        while (option != 4)
         {
-            tournament.PairTeams();
-            var teamList = tournament.teams.ToList();
-            for (int i = 0; i < teamList.Count / 2; i++)
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Simulate Group Stage");
+            Console.WriteLine("2. Simulate World cup");
+            Console.WriteLine("3. Simulate NCAA soccer");
+            Console.WriteLine("4. Exit");
+            if (int.TryParse(Console.ReadLine(), out option))
             {
-                var team = teamList[i];
-                tournament.SimulateMatch(team, team.NextRoundOpponent);
+                switch (option)
+                {
+                    case 1:
+                        tournament.SeedTeams(32);
+                        tournament.SimulateGroupStage();
+                        break;
+                    case 2:
+                        tournament.SeedTeams(16);
+                        tournament.SimulateTournament();
+                        break;
+                    case 3:
+                        tournament.SeedTeams(64);
+                        tournament.SimulateTournament();
+                        break;
+                    case 4:
+                        Console.WriteLine("Exiting the program.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please select a valid option.");
+                        break;
+                }
+
+                tournament.teams.Clear();
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid option.");
             }
         }
-
-        tournament.PathToVictory(tournament.GetTournamentWinner());
-
     }
 }
